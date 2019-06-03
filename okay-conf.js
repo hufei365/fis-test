b0fc74cf53f3fd871698b346d133dfdd3901b05a
hufei <hufeispace@gmail.com>
2019-06-03 17:57:29 +0800

    config

--- okay-conf.js
+++ okay-conf.js
@@ -26,6 +26,39 @@ fis.match('::package', {
     })
 });
 
+// 编译vue组件
+fis.match('/src/**.vue', {
+    useCompile: true, // need force specific the property  // https://github.com/fex-team/fis3/commit/eadc96bfe112d5d4cf5eb349e8a2b6e195bcb5c2
+    isMod: true,
+    // useHash: true,
+    rExt: 'js',
+    useSameNameRequire: true,
+	isPartial:false,
+    parser: [
+        (function(c, f){
+            f.isJsLike=true;
+            f.isPartial = false;
+            console.log(22222222222);
+
+            return c;
+        }),
+        // https://github.com/ccqgithub/fis3-parser-vue-component
+        fis.plugin('vue-component', {
+            runtimeOnly: true,
+            styleNameJoin: '',
+            extractCSS: false,
+            cssScopedIdPrefix: '_v-',
+            cssScopedHashType: 'sum',
+            cssScopedHashLength: 8,
+            cssScopedFlag: '__vuec__',
+			isPartial:false,
+        })
+		,fis.plugin('babel-7', {
+            sourceMap:false
+        }) // 处理ES6
+    ]
+});
+
 
 fis.match('/src/(**.{html, js, png, scss, css, jpg, vue})', {
     release :'$1',
@@ -52,6 +85,13 @@ fis.match('/node_modules/(*)/**.js', {
 
 fis.media('112').match('/src/(**.{html, js, png, scss, css, jpg, vue})', {
     release :'$1',
+    deploy: fis.plugin('http-push', {
+        receiver: 'http://10.60.0.112:7999/okay-upload',
+        to: '/xdfapp/www/xin.static.fe.xk12.cn'
+    })
+})
+.match('/src/(**).vue', {
+    release :'$1_vue.js',
     deploy: fis.plugin('http-push', {
         receiver: 'http://10.60.0.112:7999/okay-upload',
         to: '/xdfapp/www/xin.static.fe.xk12.cn'
--- src/app.vue
+++ src/app.vue
@@ -3,10 +3,10 @@
     Hello Fis
     <nav>
       <a href="/">
-        <i class="ico ico-001"></i>
+        <i class="ico ico-001"></i>UX
       </a>
       <a href="/">
-        <i class="ico ico-002"></i>
+        <i class="ico ico-002"></i>Nuts
       </a>
       <a href="/">
         <i class="ico ico-003"></i>
@@ -26,12 +26,42 @@
       <a href="/">
         <i class="ico ico-008"></i>
       </a>
+      <a href="/">
+        <i class="ico ico-009"></i>
+      </a>
     </nav>
   </div>
 </template>
 
+<style lang="scss" scoped>
+nav{
+  a{
+    color: yellowgreen;
+    text-decoration: none;
+    font-size: 2em;
+  }
+}
+.ico-009{
+    -webkit-animation: rotate 3.0s infinite linear;
+    animation: rotate 3.0s infinite linear;
+}
+
+@-webkit-keyframes rotate { 100% { -webkit-transform: rotate(360deg) }}
+@keyframes rotate { 100% { transform: rotate(360deg); -webkit-transform: rotate(360deg) }}
+</style>
+
+
 
 <script>
+Promise.resolve('resolve app.vue').then(v=>{
+  console.log(v);
+  console.log(2222);
+  console.log(3333);
+  console.log(11);
+  console.log(1);
+  console.log('asdf');
+})
 export default {};
 </script>
 
+components/theme/jquery.ui.scss
